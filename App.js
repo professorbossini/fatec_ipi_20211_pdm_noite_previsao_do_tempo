6960836f3872e00a62e8530952eeee3ada53c66b
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-03-12 19:28:13 -0300

    Adicionada a chave a um arquivo separado

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import keys from './keys';
 
 export default function App() {
+  const endPoint = `https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q=`;
+  const apiKey = keys.weatherMapApiKey;
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
